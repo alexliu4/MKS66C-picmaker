752300b493907e729b3300b00a152d7103f83717
alexliu4 <aliu9@stuy.edu>
2019-02-01 02:14:24 -0500

    working

--- picmaker.c
+++ picmaker.c
@@ -4,25 +4,52 @@
 #include<unistd.h>
 #include<string.h>
 
+// int main(){
+//   int fd, i, j;
+//   char line[20];
+//   int rgb[3];
+//
+//   fd = open("image.ppm", O_CREAT | O_TRUNC | O_WRONLY, 0644);
+//   snprintf(line, sizeof(line), "P3 500 500 255\n");
+//   write(fd, line, strlen(line));
+//   for(i=0; i<500; i++){
+//     for(j=0; j<500; j++){
+//       if (i % 2){
+//         i += 10;
+//       }
+//       rgb[0]= i % 150;
+//       if (j % 2){
+//         j += 10;
+//       }
+//       rgb[1]= j % 150;
+//       rgb[2]= 150;
+//       snprintf(line, sizeof(line), "%d %d %d ", rgb[0], rgb[1], rgb[2]);
+//       write(fd, line, strlen(line));
+//     }
+//   }
+// }
+
 int main(){
+  time_t currentTime = time(NULL);
+  srand(currentTime);
+  int color = 0;
   int fd, i, j;
   char line[20];
   int rgb[3];
-
   fd = open("image.ppm", O_CREAT | O_TRUNC | O_WRONLY, 0644);
   snprintf(line, sizeof(line), "P3 500 500 255\n");
   write(fd, line, strlen(line));
   for(i=0; i<500; i++){
     for(j=0; j<500; j++){
-      if (i % 2){
-        i += 10;
+      if (rand()%300 == 0){
+	color = 255;
       }
-      rgb[0]= i % 150;
-      if (j % 2){
-        j += 10;
+      else{
+	color = 0;
       }
-      rgb[1]= j % 150;
-      rgb[2]= 150;
+      rgb[0]= color;
+      rgb[1]= color;
+      rgb[2]= color;
       snprintf(line, sizeof(line), "%d %d %d ", rgb[0], rgb[1], rgb[2]);
       write(fd, line, strlen(line));
     }
